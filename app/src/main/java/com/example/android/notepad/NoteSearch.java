22ce5fe645ff246bdae78cecb7a6b8e8ccc5748b
1197851032 <1197851032@qq.com>
2024-12-02 13:24:07 +0800

    TSU报告

--- app/src/main/java/com/example/android/notepad/NoteSearch.java
+++ app/src/main/java/com/example/android/notepad/NoteSearch.java
@@ -20,9 +20,9 @@ public class NoteSearch extends Activity implements SearchView.OnQueryTextListen
      */
     private static final String[] PROJECTION = new String[]{
 
-            NotePad.Notes._ID, // 0
-            NotePad.Notes.COLUMN_NAME_TITLE, // 1
-            NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE//时间
+            NotePad.Notes._ID,
+            NotePad.Notes.COLUMN_NAME_TITLE,
+            NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE
     };
     @Override
     public boolean onQueryTextSubmit(String query) {
@@ -44,10 +44,8 @@ public class NoteSearch extends Activity implements SearchView.OnQueryTextListen
         }
         listView = findViewById(R.id.list_view);
         sqLiteDatabase = new NotePadProvider.DatabaseHelper(this).getReadableDatabase();
-        //Set the searchview to display the search button
         searchView.setSubmitButtonEnabled(true);
 
-        //Set the prompt text displayed by default in this searchview
         searchView.setQueryHint("search");
         searchView.setOnQueryTextListener(this);
 
@@ -59,36 +57,31 @@ public class NoteSearch extends Activity implements SearchView.OnQueryTextListen
                 "%"+string+"%","%"+string+"%"};
         Cursor cursor = sqLiteDatabase.query(
                 NotePad.Notes.TABLE_NAME,
-                PROJECTION, // The columns to return from the query
-                selection1, // The columns for the where clause
-                selection2, // The values for the where clause
-                null,          // don't group the rows
-                null,          // don't filter by row groups
-                NotePad.Notes.DEFAULT_SORT_ORDER // The sort order
+                PROJECTION,
+                selection1,
+                selection2,
+                null,
+                null,
+                NotePad.Notes.DEFAULT_SORT_ORDER
         );
-        // The names of the cursor columns to display in the view, initialized to the title column
         String[] dataColumns = {
 
                 NotePad.Notes.COLUMN_NAME_TITLE,
                 NotePad.Notes.COLUMN_NAME_MODIFICATION_DATE
         } ;
-        // The view IDs that will display the cursor columns, initialized to the TextView in
-        // noteslist_item.xml
         int[] viewIDs = {
 
                 android.R.id.text1,
                 android.R.id.text2
         };
-        // Creates the backing adapter for the ListView.
         SimpleCursorAdapter adapter
                 = new SimpleCursorAdapter(
-                this,                             // The Context for the ListView
-                R.layout.noteslist_item,         // Points to the XML for a list item
-                cursor,                           // The cursor to get items from
+                this,
+                R.layout.noteslist_item,
+                cursor,
                 dataColumns,
                 viewIDs
         );
-        // Sets the ListView's adapter to be the cursor adapter that was just created.
         listView.setAdapter(adapter);
         return true;
     }
